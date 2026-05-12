fbeb85c88987bf9dee6003dea87c1abf45f6563f
Mesut Ozkan Ertok <mesutozkanertok@gmail.com>
2026-05-12 23:59:01 +0300

    Add remote repository

---
+++ build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_tempConverterQt_FILE C:/QtRepo/tempConverterQt/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/tempConverterQt.exe)
+set(__QT_DEPLOY_TARGET_tempConverterQt_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_tempConverterQt_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/tempConverterQt_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tempConverterQt_autogen"
+  "tempConverterQt_autogen/include/ui_mainwindow.h"
+  "tempConverterQt_autogen/mocs_compilation.cpp"
+  "tempConverterQt_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tempConverterQt_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
